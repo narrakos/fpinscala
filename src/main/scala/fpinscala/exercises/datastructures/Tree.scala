b16df07b1c79b0ded0779d2f9dde041b83a64201
narrakos <narrakos96@gmail.com>
2025-09-27 19:15:08 +0200

    Exercise 3.25 - 3.28

--- src/main/scala/fpinscala/exercises/datastructures/Tree.scala
+++ src/main/scala/fpinscala/exercises/datastructures/Tree.scala
@@ -8,17 +8,23 @@ enum Tree[+A]:
     case Leaf(_) => 1
     case Branch(l, r) => 1 + l.size + r.size
 
-  def depth: Int = ???
+  def depth: Int = this match
+    case Leaf(_) => 0
+    case Branch(left, right) => 1 + (left.depth).max(right.depth)
 
-  def map[B](f: A => B): Tree[B] = ???
+  def map[B](f: A => B): Tree[B] = this match
+    case Leaf(value) => Leaf(f(value))
+    case Branch(left, right) => Branch(left.map(f), right.map(f))
 
-  def fold[B](f: A => B, g: (B,B) => B): B = ???
+  def fold[B](f: A => B, g: (B,B) => B): B = this match
+    case Leaf(value) => f(value)
+    case Branch(left, right) => g(left.fold(f, g), right.fold(f, g))
+
+  def sizeViaFold: Int = this.fold(_ => 1, _+_+1)
   
-  def sizeViaFold: Int = ???
+  def depthViaFold: Int = this.fold(_ => 0, _.max(_)+1)
   
-  def depthViaFold: Int = ???
-  
-  def mapViaFold[B](f: A => B): Tree[B] = ???
+  def mapViaFold[B](f: A => B): Tree[B] = this.fold(l => Leaf(f(l)), (l, r) =>Branch(l, r))
 
 object Tree:
 
@@ -26,8 +32,13 @@ object Tree:
     case Leaf(_) => 1
     case Branch(l,r) => 1 + size(l) + size(r)
 
-  extension (t: Tree[Int]) def firstPositive: Int = ???
+  extension (t: Tree[Int]) def firstPositive: Int = t match
+    case Leaf(value) => value
+    case Branch(l, r) => if l.firstPositive > 0 then l.firstPositive else r.firstPositive
 
-  extension (t: Tree[Int]) def maximum: Int = ???
+  extension (t: Tree[Int]) def maximum: Int =
+      t match
+        case Leaf(v) => v
+        case Branch(left, right) => left.maximum.max(right.maximum)
 
-  extension (t: Tree[Int]) def maximumViaFold: Int = ???
+  extension (t: Tree[Int]) def maximumViaFold: Int = t.fold(v => v, (l,r) => l.max(r))
